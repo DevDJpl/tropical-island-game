2a9ccd83fb2337687123a7ff7cf4695af9d5c0ea
”Dziedzina_Karolina” <mail@example.com>
2023-11-14 00:52:00 +0100

    Purchase of buildings

--- Game/Assets/Scripts/game/Game.cs
+++ Game/Assets/Scripts/game/Game.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     public GameObject walecNie;
     public GameObject walecTak;
+    float CurrentBalance;
+    float BaseStoreCost;
+
+    public Text CurrentBalanceText;
     // Start is called before the first frame update
     void Start()
     {
         walecTak.SetActive(false);
+        CurrentBalance = 8;
+        BaseStoreCost = 1;
+        CurrentBalanceText.text = CurrentBalance.ToString();
     }
     
     // Update is called once per frame
@@ -23,8 +31,13 @@ public class Game : MonoBehaviour
             {
                 if (hit.collider.gameObject == walecNie)
                 {
+                    if (BaseStoreCost > CurrentBalance)
+                        return;
                     walecTak.SetActive(true);
                     walecNie.SetActive(false);
+                    CurrentBalance = CurrentBalance - BaseStoreCost;
+                    Debug.Log(CurrentBalance);
+                    CurrentBalanceText.text = CurrentBalance.ToString();
                 }
             }
         }
